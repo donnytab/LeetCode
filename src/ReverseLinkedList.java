24d02a1d45e00e34bcdea50d24253f2b5ba65500
downson <downson.yuen@gmail.com>
2018-06-05 23:54:20 -0400

    Reverse linked list

---
+++ src/ReverseLinkedList.java
@@ -0,0 +1,24 @@
+
+public class ReverseLinkedList {
+	 public class ListNode {
+		      int val;
+		      ListNode next;
+		      ListNode(int x) { val = x; }
+		  }
+	
+	public ListNode reverseList(ListNode head) {
+		if(head == null || head.next == null) {
+			return null;
+		}
+		
+		ListNode prev = null, current = head;
+		
+		while(current != null && current.next != null) {
+			prev = current.next;
+			current.next = current.next.next;
+			prev.next = head;
+			head = prev;
+		}
+		return head;
+	}
+}
