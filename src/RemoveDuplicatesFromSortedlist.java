27531f02bd265956597721da24121cacff93603c
downson <downson.yuen@gmail.com>
2018-06-28 23:27:32 -0400

    Remove duplicates from sorted list

---
+++ src/RemoveDuplicatesFromSortedlist.java
@@ -0,0 +1,21 @@
+
+public class RemoveDuplicatesFromSortedlist {
+	public class ListNode {
+		int val;
+		ListNode next;
+		ListNode(int value) {val = value;}
+	}
+	
+	public ListNode deleteDuplicates(ListNode head) {
+		ListNode current = head;
+		while(current != null && current.next != null) {
+			if(current.val == current.next.val) {
+				current.next = current.next.next;
+			}
+			else {
+				current = current.next;
+			}
+		}
+		return head;
+	}
+}
