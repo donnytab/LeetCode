d465fe06fe1bc57367271af88d632b9bcf799e1d
downson <downson.yuen@gmail.com>
2018-06-28 23:15:00 -0400

    Reverse Linked list - Recursion solution

--- src/ReverseLinkedList.java
+++ src/ReverseLinkedList.java
@@ -6,19 +6,48 @@ public class ReverseLinkedList {
 		      ListNode(int x) { val = x; }
 		  }
 	
-	public ListNode reverseList(ListNode head) {
-		if(head == null || head.next == null) {
-			return head;
-		}
-		
-		ListNode prev = null, current = head;
-		
-		while(current != null && current.next != null) {
-			prev = current.next;
-			current.next = current.next.next;
-			prev.next = head;
-			head = prev;
-		}
-		return head;
-	}
+	// Iteration solution
+//	public ListNode reverseList(ListNode head) {
+//		if(head == null || head.next == null) {
+//			return head;
+//		}
+//		
+//		ListNode prev = null, current = head;
+//		
+//		while(current != null && current.next != null) {
+//			prev = current.next;
+//			current.next = current.next.next;
+//			prev.next = head;
+//			head = prev;
+//		}
+//		return head;
+//	}
+	 
+	 public ListNode reverseList(ListNode head) {
+		 ListNode currentNode = head, nextNode = null, prevNode = null;
+		 while(currentNode != null) {
+			 nextNode = currentNode.next;
+			 currentNode.next = prevNode;
+			 prevNode = currentNode;
+			 currentNode = nextNode;
+		 }
+		 head = prevNode;
+		 return head;
+	 }
+	
+	// Recursion solution
+	/*
+	 public ListNode reverseList(ListNode head) {
+	 	return reverseListInt(head, null);
+	 }
+	 
+	 public ListNode reverseListInt(ListNode head, ListNode newHead) {
+	 	if(head == null) {
+	 		return newHead;
+	 	}
+	 	ListNode next = head.next;
+	 	head.next = newHead;
+	 	return reverseListInt(next, head);
+	 }
+	 */
 }
