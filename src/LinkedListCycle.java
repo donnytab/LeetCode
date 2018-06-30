9a2b5156e3b7211fe918298516d8ad85a5a68dd7
downson <downson.yuen@gmail.com>
2018-06-29 22:03:38 -0400

    Linked list cycle

---
+++ src/LinkedListCycle.java
@@ -0,0 +1,24 @@
+
+public class LinkedListCycle {
+	class ListNode{
+		int val;
+		ListNode next;
+		ListNode(int value) {val = value; next = null;}
+	}
+	
+	public boolean hasCycle(ListNode head) {
+		if(head == null) {
+			return false;
+		}
+		
+		ListNode fast = head, slow = head;
+		while(fast.next != null && fast.next.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+			if(slow.val == fast.val) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
