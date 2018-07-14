d70db3804024cb0766c81d32507fab093893312a
downson <downson.yuen@gmail.com>
2018-07-13 23:55:19 -0400

    Detect Capital with Regex

--- src/DetectCapital.java
+++ src/DetectCapital.java
@@ -1,11 +1,19 @@
 
 public class DetectCapital {
-	public boolean detectCapitalUse(String word) {
+	public static boolean detectCapitalUse(String word) {
 		char[] temp = word.toCharArray();
 		int capital = 0;
 		for(char c : temp) {
 			if('Z' - c >= 0) capital++;
 		}
 		return capital==temp.length || capital==0 || (capital==1 && 'Z' - temp[0] >= 0);
+	}
+	
+	public static boolean detectCapitalUseWithRegex(String word) {
+		return word.matches("[A-Z]+|[a-z]+|[A-Z][a-z]+");
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(detectCapitalUseWithRegex("USA"));
 	}
 }
