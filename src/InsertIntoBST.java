51e7affd6f76214618f563a9e59694452aab04db
downson <downson.yuen@gmail.com>
2018-08-24 23:41:01 -0400

    Insert into BST

---
+++ src/InsertIntoBST.java
@@ -0,0 +1,19 @@
+
+public class InsertIntoBST {
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode(int x) {val = x;}
+	}
+	
+	public TreeNode insertIntoBST(TreeNode root, int val) {
+		if(root == null) return new TreeNode(val);
+		if(root.val < val) {
+			root.right = insertIntoBST(root.right, val);
+		} else {
+			root.left = insertIntoBST(root.left, val);
+		}
+		return root;
+	}
+}
