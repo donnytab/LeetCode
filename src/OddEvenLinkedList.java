6bfc27293302f7c6f9de59d1f12121cbd03c1d73
downson <downson.yuen@gmail.com>
2018-08-29 23:50:49 -0400

    Odd even linked list

---
+++ src/OddEvenLinkedList.java
@@ -0,0 +1,29 @@
+/*
+ * Please note here we are talking about the node number and not the value in the nodes.
+Input: 1->2->3->4->5->NULL
+Output: 1->3->5->2->4->NULL
+
+Input: 2->1->3->5->6->4->7->NULL
+Output: 2->3->6->7->1->5->4->NULL
+ */
+public class OddEvenLinkedList {
+	public class ListNode {
+		int val;
+		ListNode next;
+		ListNode(int x) {val = x;}
+	}
+	
+	public ListNode oddEvenList(ListNode head) {
+		if(head != null) {
+			ListNode odd = head, even = head.next, evenHead = even;
+			while(even != null && even.next != null) {
+				odd.next = odd.next.next;
+				even.next = even.next.next;
+				odd = odd.next;
+				even = even.next;
+			}
+			odd.next = evenHead;
+		}
+		return head;
+	}
+}
