6e9863873e901d475d59d869638576484b8bb0aa
downson <downson.yuen@gmail.com>
2018-08-30 23:55:18 -0400

    Binary tree level order traversal with Queue

--- src/BinaryTreeLevelOrderTraversal.java
+++ src/BinaryTreeLevelOrderTraversal.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class BinaryTreeLevelOrderTraversal {
 	public class TreeNode {
@@ -25,5 +26,26 @@ public class BinaryTreeLevelOrderTraversal {
 		list.get(height).add(root.val);
 		levelHelper(list, root.left, height+1);
 		levelHelper(list, root.right, height+1);
+	}
+	
+	// Queue solution
+	public List<List<Integer>> levelOrderQueue(TreeNode root) {
+		Queue<TreeNode> queue = new LinkedList<TreeNode>();
+		List<List<Integer>> resultList = new LinkedList<List<Integer>>();
+		
+		if(root==null) return resultList;
+		
+		queue.offer(root);
+		while(!queue.isEmpty()){
+			int levelNum = queue.size();
+			List<Integer> subList = new LinkedList<>();
+			for(int i=0; i<levelNum; i++) {
+				if(queue.peek().left != null) queue.offer(queue.peek().left);
+				if(queue.peek().right != null) queue.offer(queue.peek().right);
+				subList.add(queue.poll().val);
+			}
+			resultList.add(subList);
+		}
+		return resultList;
 	}
 }
