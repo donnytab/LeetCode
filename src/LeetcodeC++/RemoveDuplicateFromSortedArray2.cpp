2a095b8ca0c150820826f780f30a2c6c879172b8
downson <downson.yuen@gmail.com>
2018-09-18 23:43:52 -0400

    Remove duplicate from sorted array 2 (C++)

---
+++ src/LeetcodeC++/RemoveDuplicateFromSortedArray2.cpp
@@ -0,0 +1,21 @@
+/*
+Given a sorted array nums, remove the duplicates in-place such that duplicates appeared at most twice and return the new length.
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+ */
+
+#include <vector>
+
+using namespace std;
+
+class RemoveDuplicateFromSortedArray2 {
+public:
+    // Sample solution
+    int removeDuplicates(vector<int>& nums) {
+        int i = 0;
+        for (int n : nums)
+            if (i < 2 || n > nums[i-2])
+                nums[i++] = n;
+        return i;
+    }
+};
