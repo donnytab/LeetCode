13e3d8744ad03229f3ab2aad74c785abc074f19b
downson <downson.yuen@gmail.com>
2019-01-29 23:57:52 -0500

    Fraction to decimal

---
+++ src/FractionDecimal.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+
+public class FractionDecimal {
+	public String fractionToDecimal(int numerator, int denominator) {
+		if(numerator == 0) return "0";
+		StringBuilder sb = new StringBuilder();
+		sb.append(((numerator > 0) ^ (denominator > 0)) ? "-" : "");
+        long num = Math.abs((long)numerator);
+        long den = Math.abs((long)denominator);
+        // integral
+        sb.append(num/den);
+        num %= den;
+        if(num == 0) return sb.toString();
+        // fraction
+        sb.append(".");
+        HashMap<Long, Integer> map = new HashMap<Long, Integer>();
+        map.put(num, sb.length());
+        while(num != 0) {
+        	num *= 10;
+            sb.append(num/den);
+            num %= den;
+            if(map.containsKey(num)) {
+            	int index = map.get(num);
+            	sb.insert(index, "(");
+            	sb.append(")");
+            	break;
+            } else {
+            	map.put(num, sb.length());
+            }
+        }
+        return sb.toString();
+	}
+}
